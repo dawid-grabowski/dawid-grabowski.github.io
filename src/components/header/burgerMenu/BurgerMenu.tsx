04d41096bd31f8f31132c765cc2cceabc2e1783c
Dawid Grabowski <kontakt@grabowskidawid.com>
2022-10-19 22:54:20 +0200

    add: burger menu css

---
+++ src/components/header/burgerMenu/BurgerMenu.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './burger.css';
+
+const BurgerMenu: React.FC = (): React.ReactElement => {
+	return <div className='burger-menu'></div>;
+};
+
+export default BurgerMenu;
