c1aa4b2201ea23e51164364209de62d2806951a2
Dawid Grabowski <kontakt@grabowskidawid.com>
2022-10-19 11:31:50 +0200

    add: FAQ panel

---
+++ src/utils/faqData.ts
@@ -0,0 +1,19 @@
+export interface IFaqData {
+	title: string;
+	description: string;
+}
+
+export const faqData: IFaqData[] = [
+	{
+		title: 'Arcu non odio',
+		description: 'Sapien nec sagittis aliquam malesuada bibendum. Tempor nec feugiat nisl pretium.',
+	},
+	{
+		title: 'Eget gravida cum sociis',
+		description: 'Sapien nec sagittis aliquam malesuada bibendum. Tempor nec feugiat nisl pretium.',
+	},
+	{
+		title: 'Massa massa ultricies',
+		description: 'Sapien nec sagittis aliquam malesuada bibendum. Tempor nec feugiat nisl pretium.',
+	},
+];
